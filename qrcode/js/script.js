5b5a2a33502e0118c1220ec2b033ba8ad81bf4fc
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2025-05-28 15:51:29 -0300

    Update script.js

--- qrcode/js/script.js
+++ qrcode/js/script.js
@@ -3,16 +3,25 @@ const botao = document.querySelector('#botao')
 const qr = document.querySelector('#qrcode')
 
 botao.addEventListener('click', gerarQR)
+url.addEventListener('keydown', (event)=> {
+    if (event.key == 'Enter') {
+        gerarQR()
+    }
+})
 
 function gerarQR() {
     texto = url.value
-    qr.innerHTML = ""
 
-    code = new QRCode (qr, {
-        text: texto,
-        width: 400,
-        height: 400,
-        colorDark: 'white',
-        colorLight: 'black'
-    });
+    if (texto) {
+        code = new QRCode (qr, {
+            text: texto,
+            width: 400,
+            height: 400,
+            colorDark: 'white',
+            colorLight: 'black'
+        })
+    } 
+    else {
+        qr.innerHTML = ''
+    }
 }
